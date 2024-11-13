2eeece646f52d924e5c0a0f33ae573cbb9a78914
Steve White <stevemichaelwhite@gmail.com>
2024-11-13 18:27:07 +1300

    Added player movement

--- src/player.rs
+++ src/player.rs
@@ -5,7 +5,54 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player);
+        app.add_systems(Startup, spawn_player).add_systems( Update, player_movement);
+    }
+}
+
+#[derive(Component)]
+struct Player;
+
+fn player_movement(
+    keys: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut player_q: Query<&mut Transform, With<Player>>,
+    cam_q: Query<&Transform, (With<Camera3d>, Without<Player>)>
+
+) {
+    for mut player_transform in player_q.iter_mut() {
+        let cam = match cam_q.get_single() {
+            Ok(c) => c,
+            Err(e) => Err(format!("Error retrieving camera: {}", e)).unwrap()
+        };
+
+        let mut direction = Vec3::ZERO;
+
+        //forward
+        if keys.pressed(KeyCode::W) {
+            direction += cam.forward();
+        }
+
+        //back
+        if keys.pressed(KeyCode::S) {
+            direction += cam.back();
+        }
+
+        //left
+        if keys.pressed(KeyCode::A) {
+            direction += cam.left();
+        }
+
+        //back
+        if keys.pressed(KeyCode::D) {
+            direction += cam.right();
+        }
+
+        direction.y = 0.0;
+
+        let movement = direction.normalize_or_zero() * 2.0 * time.delta_seconds();
+        player_transform.translation += movement;
+
+
     }
 }
 
@@ -14,11 +61,11 @@ fn spawn_player(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let player = PbrBundle {
+    let player = (PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
         material: materials.add(Color::BLUE.into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
-    };
+    }, Player);
     commands.spawn(player);
 }
