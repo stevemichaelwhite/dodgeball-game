ade9aa85eb81a30050d58497b73a4e160e069841
Steve White <stevemichaelwhite@gmail.com>
2024-11-14 20:45:01 +1300

    created cube closure

--- src/world.rs
+++ src/world.rs
@@ -4,7 +4,15 @@ pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (spawn_light, spawn_directional_light, spawn_floor, spawn_objects));
+        app.add_systems(
+            Startup,
+            (
+                spawn_light,
+                spawn_directional_light,
+                spawn_floor,
+                spawn_objects,
+            ),
+        );
     }
 }
 
@@ -68,36 +76,37 @@ fn spawn_objects(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let blue_cube = (
-        PbrBundle {
-            mesh: meshes.add(Extrusion::new(Rectangle::default(), 4.)),
-            material: materials.add(StandardMaterial {
-                base_color: Srgba::hex("#1a1fad").unwrap().into(),
-                metallic: 0.620,
-                perceptual_roughness: 0.8,
-                ..default()
-            }),
-            transform: Transform::from_xyz(1.7, 0.5, 0.0),
-            ..default()
-        },
-        Name::new("BlueCube"),
-    );
 
-    let red_cube = (
-        PbrBundle {
-            mesh: meshes.add(Extrusion::new(Rectangle::default(), 2.)),
-            material: materials.add(StandardMaterial {
-                base_color: Srgba::hex("#ad1a30").unwrap().into(),
-                metallic: 0.620,
-                perceptual_roughness: 0.8,
-                ..default()
-            }),
-            transform: Transform::from_xyz(-3.3, 0.5, 3.5),
-            ..default()
-        },
-        Name::new("RedCube"),
-    );
 
-    commands.spawn(blue_cube);
-    commands.spawn(red_cube);
+    let mut create_cube =
+        |depth: f32, color_hex: String, xyz: (f32, f32, f32), name: String| -> (PbrBundle, Name) {
+            (
+                PbrBundle {
+                    mesh: meshes.add(Extrusion::new(Rectangle::default(), depth)),
+                    material: materials.add(StandardMaterial {
+                        base_color: Srgba::hex(color_hex).unwrap().into(),
+                        metallic: 0.620,
+                        perceptual_roughness: 0.8,
+                        ..default()
+                    }),
+                    transform: Transform::from_xyz(xyz.0, xyz.1, xyz.2),
+                    ..default()
+                },
+                Name::new(name),
+            )
+        };
+    commands.spawn(create_cube(
+        4.0,
+        "#1a1fad".to_string(),
+        (1.7, 0.5, 0.0),
+        "BlueCube".to_string(),
+    ));
+
+    commands.spawn(create_cube(
+        2.0,
+        "#ad1a30".to_string(),
+        (-3.3, 0.5, 3.5),
+        "RedCube".to_string(),
+    ));
+
 }
