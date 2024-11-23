1dc68c32aa4c430936add5a931276fd872d75c82
Steve White <stevemichaelwhite@gmail.com>
2024-11-23 17:21:21 +1300

    player movement works again

--- src/player.rs
+++ src/player.rs
@@ -3,10 +3,10 @@ use bevy::{input::InputSystem, prelude::*};
 use bevy_rapier3d::prelude::*;
 use bevy_third_person_camera::*;
 
-const GROUND_TIMER: f32 = 0.5;
+// const GROUND_TIMER: f32 = 0.5;
 const MOVEMENT_SPEED: f32 = 8.0;
-const JUMP_SPEED: f32 = 20.0;
-const GRAVITY: f32 = -9.81;
+// const JUMP_SPEED: f32 = 20.0;
+// const GRAVITY: f32 = -9.81;
 
 pub struct PlayerPlugin;
 
@@ -14,8 +14,8 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<MovementInput>()
             .add_systems(Startup, spawn_player)
-            .add_systems(PreUpdate, handle_input.after(InputSystem))
-            .add_systems(FixedUpdate, player_movement);
+            // .add_systems(PreUpdate, handle_input.after(InputSystem))
+            .add_systems(Update, player_movement);
     }
 }
 
@@ -26,41 +26,38 @@ struct Player;
 #[derive(Default, Resource, Deref, DerefMut)]
 struct MovementInput(Vec3);
 
-fn handle_input(
-    keyboard: Res<ButtonInput<KeyCode>>,
-    mut movement: ResMut<MovementInput>,
-    // mut look: ResMut<LookInput>,
-    // mut mouse_events: EventReader<MouseMotion>,
-) {
-    if keyboard.pressed(KeyCode::KeyW) {
-        movement.z -= 1.0;
-    }
-    if keyboard.pressed(KeyCode::KeyS) {
-        movement.z += 1.0
-    }
-    if keyboard.pressed(KeyCode::KeyA) {
-        movement.x -= 1.0;
-    }
-    if keyboard.pressed(KeyCode::KeyD) {
-        movement.x += 1.0
-    }
-    **movement = movement.normalize_or_zero();
-    if keyboard.pressed(KeyCode::ShiftLeft) {
-        **movement *= 2.0;
-    }
-    // if keyboard.pressed(KeyCode::Space) {
-    //     movement.y = 1.0;
-    // }
+// fn handle_input(
+//     keyboard: Res<ButtonInput<KeyCode>>,
+//     mut movement: ResMut<MovementInput>,
+//     cam_q: Query<&Transform, (With<Camera3d>, Without<Player>)>,
+// ) {
+//     let cam = match cam_q.get_single() {
+//         Ok(c) => c,
+//         Err(e) => Err(format!("Error retrieving camera: {}", e)).unwrap(),
+//     };
 
-    // for event in mouse_events.read() {
-    //     look.x -= event.delta.x * MOUSE_SENSITIVITY;
-    //     look.y -= event.delta.y * MOUSE_SENSITIVITY;
-    //     look.y = look.y.clamp(-89.9, 89.9); // Limit pitch
-    // }
-}
+//     let mut direction = Vec3::ZERO;
+//     if keyboard.pressed(KeyCode::KeyW) {
+//         direction -= *cam.forward();
+//     }
+//     if keyboard.pressed(KeyCode::KeyS) {
+//         direction += *cam.back();
+//     }
+//     if keyboard.pressed(KeyCode::KeyA) {
+//         direction -= *cam.left();
+//     }
+//     if keyboard.pressed(KeyCode::KeyD) {
+//         direction += *cam.right();
+//     }
+//     **movement = direction.normalize_or_zero();
+//     if keyboard.pressed(KeyCode::ShiftLeft) {
+//         **movement *= 2.0;
+//     }
+
+// }
 
 fn player_movement(
-    mut input: ResMut<MovementInput>,
+    keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
     mut player_q: Query<
         (
@@ -71,53 +68,55 @@ fn player_movement(
         With<Player>,
     >,
     cam_q: Query<&Transform, (With<Camera3d>, Without<Player>)>,
-    mut vertical_movement: Local<f32>,
-    mut grounded_timer: Local<f32>,
+
 ) {
-    for (mut player_transform, mut controller, output) in player_q.iter_mut() {
+
+    
+
+    for (mut player_transform, mut _controller, _output) in player_q.iter_mut() {
+
+
         let cam = match cam_q.get_single() {
             Ok(c) => c,
             Err(e) => Err(format!("Error retrieving camera: {}", e)).unwrap(),
         };
 
 
-        // input.y = 0.0;
+        let mut direction = Vec3::ZERO;
+
+        //forward
+        if keys.pressed(KeyCode::KeyW) {
+            direction += *cam.forward();
+        }
+
+        //back
+        if keys.pressed(KeyCode::KeyS) {
+            direction += *cam.back();
+        }
+
+        //left
+        if keys.pressed(KeyCode::KeyA) {
+            direction += *cam.left();
+        }
+
+        //back
+        if keys.pressed(KeyCode::KeyD) {
+            direction += *cam.right();
+        }
+
+        direction.y = 0.0;
         let delta_time = time.delta_seconds();
         // Retrieve input
-        let mut movement = Vec3::new(input.x, 0.0, input.z) * MOVEMENT_SPEED;
-        // let jump_speed = input.y * JUMP_SPEED;
+     
+        let movement =  direction.normalize_or_zero() * MOVEMENT_SPEED * delta_time;
+  
 
-        // Clear input
-        **input = Vec3::ZERO;
-
-        // Check physics ground check
-        // if output.map(|o| o.grounded).unwrap_or(false) {
-        //     *grounded_timer = GROUND_TIMER;
-        //     *vertical_movement = 0.0;
-        // }
-
-        // If we are grounded we can jump
-        // if *grounded_timer > 0.0 {
-        //     *grounded_timer -= delta_time;
-        //     // If we jump we clear the grounded tolerance
-        //     if jump_speed > 0.0 {
-        //         *vertical_movement = jump_speed;
-        //         *grounded_timer = 0.0;
-        //     }
-        // }
-
-        movement.y = *vertical_movement;
-        movement.y = 0.0;
-        // *vertical_movement += GRAVITY * delta_time * controller.custom_mass.unwrap_or(1.0);
-        controller.translation = Some(player_transform.rotation * (movement * delta_time));
-
-        // let movement = input.normalize_or_zero() * player_speed.0 * time.delta_seconds();
-        // player_transform.translation += movement;
+        player_transform.translation += movement;
 
         //
-        // if input.length_squared() > 0.0 {
-        //     player_transform.look_to(input, Vec3::Y)
-        // }
+        if direction.length_squared() > 0.0 {
+            player_transform.look_to(direction, Vec3::Y)
+        }
     }
 }
 
